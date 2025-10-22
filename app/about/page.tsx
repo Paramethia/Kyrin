0e80de53963852244b183ca378b629f2f09eaa18
Kyrin <kyrinkompi@gmail.com>
2025-10-22 18:05:06 +0200

    Update
    - Improved light mode
    - Added new link to invicon website

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,13 +15,13 @@ export default function About() {
 
         <motion.div initial={{ y: -20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.6, duration: 0.5 }} className="space-y-6">
           <div className="p-6 border border-gray-700/20 rounded-xl backdrop-blur-sm hover:border-gray-700/50 transition-colors duration-300">
-            <h2 className="text-2xl text-gray-700 dark:text-gray-200 font-semibold mb-2">Me</h2>
+            <h2 className="text-2xl text-gray-600 dark:text-gray-200 font-semibold mb-2">Me</h2>
             <p className="text-gray-600 dark:text-gray-400 mb-4">
               I am 21 years old from Free State, South Africa. I've always been interested in technology and how things work as they do. When it comes to video games, I'm a huge fan of God of War, minecraft, COD and some other games. I code, do voices, listen to music and a few other things. My favorite artist is XXXTENTACION.
             </p>
           </div>
           <div className="p-6 border border-gray-700/20 rounded-xl backdrop-blur-sm hover:border-gray-700/50 transition-colors duration-300">
-            <h2 className="text-2xl text-gray-500 dark:text-gray-200 font-semibold mb-2">Journey as a developer</h2>
+            <h2 className="text-2xl text-gray-600 dark:text-gray-200 font-semibold mb-2">Journey as a developer</h2>
             <p className="text-gray-600 dark:text-gray-400 mb-4">
               I started coding in 2023 around August. I was first introduced to HTML from school, then I became hooked and wanted to learn more. I've been continuously learning and improving my skills in web development. Still doing so right now, actually. I also make applications for mobile (just android for now) and computers.
             </p>
